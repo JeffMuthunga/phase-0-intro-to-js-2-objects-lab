4e3541c85ac4f98e9bbcf8a928e9962da4b7f8e3
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-11-19 20:51:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jeff",
+    streetAddress: "2664-Ngoingwa"
+}
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    const newObj = {
+        ...employee, [key]: value
+    };
+    return employee, newObj;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
